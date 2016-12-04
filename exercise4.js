72b813252dff5ec08b3c12404a46ed7b4e282fe0
jtlong3rd <jtlong3rd@gmail.com>
2016-12-04 13:20:12 -0500

    Complete Exercise 4

---
+++ exercise4.js
@@ -0,0 +1,30 @@
+//  FIND PROJECT
+//  Exercise 4 of 9
+
+// Here we will learn how to search for documents but only fetch the fields
+// we need. Also known as projection in MongoDB
+
+// Use the parrots collection from the database named learnyoumongo to
+// find all documents where age is greater than the first argument
+// passed to your script.
+
+// The difference from the last lesson will be that we only want the
+// name and age properties
+
+// Using console.log, print the documents to stdout.
+
+const mongo = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/learnyoumongo';
+const ageBound = +process.argv[2];
+
+mongo.connect(url, function(err, db) {
+  const oldParrots = db
+    .collection('parrots')
+    .find(
+      { age: { $gt: ageBound } },
+      { name: 1, age: 1, _id: 0 })
+    .toArray()
+    .then(oldParrots => console.log(oldParrots));
+
+  db.close();
+});
