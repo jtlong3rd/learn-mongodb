9f54afb1e210180b1bc892244cb818db0c63527c
jtlong3rd <jtlong3rd@gmail.com>
2016-12-04 14:12:23 -0500

    Refactor Exercise 3 to use promises

--- exercise3.js
+++ exercise3.js
@@ -15,7 +15,7 @@ const mongo = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017/learnyoumongo';
 const ageBound = +process.argv[2];
 
-mongo.connect(url, function(err, db) {
+mongo.connect(url).then(db => {
   db.collection('parrots')
     .find({ age: { $gt: ageBound } })
     .toArray()
