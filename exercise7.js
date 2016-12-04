e732520a1c9a6677430ff91d1ac4092d5044e2e8
jtlong3rd <jtlong3rd@gmail.com>
2016-12-04 13:41:40 -0500

    Complete Exercise 7

---
+++ exercise7.js
@@ -0,0 +1,23 @@
+//  REMOVE
+//  Exercise 7 of 9
+
+// This lesson involves removing a document with the given _id.
+
+// The database name will be accessible via process.argv[2].
+
+// The collection name will be passed as the second argument to your script.
+
+// The _id will be passed as the third argument to your script.
+
+const mongo = require('mongodb').MongoClient;
+const dbName = process.argv[2];
+const collectionName = process.argv[3];
+const _id = process.argv[4];
+const url = `mongodb://localhost:27017/${dbName}`;
+
+mongo.connect(url, function(err, db) {
+  db.collection(collectionName)
+    .remove({ _id });
+
+  db.close();
+});
