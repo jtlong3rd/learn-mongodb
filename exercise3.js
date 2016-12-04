7cd0570ab6e9ecb617301de07e3aab94be6ea032
jtlong3rd <jtlong3rd@gmail.com>
2016-12-04 13:11:17 -0500

    Complete Exercise 3

---
+++ exercise3.js
@@ -0,0 +1,26 @@
+//  FIND
+//  Exercise 3 of 9
+
+// Here we will learn how to search for documents.
+
+// In this exercise the database name is learnyoumongo.
+// So, the url would be something like: mongodb://localhost:27017/learnyoumongo
+
+// Use the parrots collection to find all documents where age
+// is greater than the first argument passed to your script.
+
+// Using console.log, print the documents to stdout.
+
+const mongo = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/learnyoumongo';
+const ageBound = +process.argv[2];
+
+mongo.connect(url, function(err, db) {
+  const oldParrots = db
+    .collection('parrots')
+    .find({ age: { $gt: ageBound } })
+    .toArray()
+    .then(oldParrots => console.log(oldParrots));
+
+  db.close();
+});
